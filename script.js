ad8f038c6e9f5cd27082f000e9f63d95e9d4d8e9
einobaka <61556425+einobaka@users.noreply.github.com>
2020-04-24 22:48:39 -0700

    setup script.js with variable to be used with index.html

---
+++ script.js
@@ -0,0 +1,67 @@
+var inPlayer = document.querySelector("#player");
+var inSubmit = document.querySelector("#submit");
+var outHiTimer = document.querySelector("#timer");
+var outScore = document.querySelector("#score");
+var outHiScore = document.querySelector("#hiscore");
+var outQuestion = document.querySelector("#question");
+var outAnswer = document.querySelector("#answer");
+
+
+
+var firstQuestion = outQuestion;
+var firstQuestion = document.createElement("p");
+var testVariables = {
+    varOne: "one",
+    varTwo: "two",
+};
+firstQuestion.textContent = [testVariables.varOne, testVariables.varTwo];
+outQuestion.append(firstQuestion);
+
+
+
+console.log(firstQuestion);
+
+
+
+
+//Grab player information and store it later
+// inSubmit.addEventListener("click", askName);
+// function askName() {
+//     var playerName = prompt("Please enter your name.");
+//     return inPlayer.textContent = playerName, inSubmit.textContent = "RESTART GAME", console.log(playerName), qAndA();
+// };
+
+//create questions and they have to be random, maybe create another questions page??
+
+// inSubmit.addEventListener("click", qAndA);
+// function qAndA() {
+//     firstQuestion = '';
+
+//     firstQuestion = document.createElement("button"),
+//     firstQuestion.textContent = "test again",
+//     console.log("this is a test");
+
+//     // outAnswer.textContent = [q1.a2, q1.a3]; 
+//     //the answer can come from a different webpage
+// };
+
+// fill a variable with button on HTML page
+
+// var q1 = {
+//     q: "question example 1",
+//     a1: "answer 1",
+//     a2: "blueblueblue2",
+//     a3: "blueblueblue3",
+// }
+
+
+
+
+
+//figure this out later
+// localStorage.setItem("player", inPlayer);
+//Replace click to start with "RESTART GAME"
+
+//Countdown of questions next to question?
+
+// have an empty bar that flashes correct or incorrect?
