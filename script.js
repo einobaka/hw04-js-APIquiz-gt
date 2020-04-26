f30f7e20a746bbe0408bcbbcc6de60421bc859e6
einobaka <61556425+einobaka@users.noreply.github.com>
2020-04-26 11:08:39 -0700

    added more qAndA functions for storing questions and answers

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var outQuestion = document.querySelector("#question");
 var outAnswer = document.querySelector("#answer");
 var outResult = document.querySelector("#result");
 
+
 //create buttons for answers
 var qAndA = {
     firstQuestion: "",
@@ -25,38 +26,86 @@ var qAndA = {
         return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
     },
 
-};
+    qAndATwo: function () {
+        qAndA.firstQuestion = "question test 2 ";
+        qAndA.firstAnswer = "answer 1 3 ";
+        qAndA.secondAnswer = "answer 2 4 ";
+        qAndA.thirdAnswer = "answer 3 5 ";
+        qAndA.fourthAnswer = "answer 4 6 ";
 
-console.log(qAndA.qAndAOne())
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+    },
+
+    qAndAThree: function () {
+        qAndA.firstQuestion = "question test ";
+        qAndA.firstAnswer = "answer 1 ";
+        qAndA.secondAnswer = "answer 2 ";
+        qAndA.thirdAnswer = "answer 3 ";
+        qAndA.fourthAnswer = "answer 4 ";
+
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+    },
+
+    qAndAFour: function () {
+        qAndA.firstQuestion = "question test ";
+        qAndA.firstAnswer = "answer 1 ";
+        qAndA.secondAnswer = "answer 2 ";
+        qAndA.thirdAnswer = "answer 3 ";
+        qAndA.fourthAnswer = "answer 4 ";
+
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+    },
+
+    qAndAFive: function () {
+        qAndA.firstQuestion = "question test ";
+        qAndA.firstAnswer = "answer 1 ";
+        qAndA.secondAnswer = "answer 2 ";
+        qAndA.thirdAnswer = "answer 3 ";
+        qAndA.fourthAnswer = "answer 4 ";
+
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+    },
+
+};
+console.log(qAndA.qAndATwo())
+
+
 
 //answer block to be re-used//
-outQuestion.textContent = qAndA.firstQuestion;
-outQuestion.setAttribute("class", "list-group-item list-group-item-info");
-console.log(outQuestion);
 
-answerList1 = document.createElement("li");
-answerList1.textContent = qAndA.firstAnswer;
-answerList1.setAttribute("class", "list-group-item list-group-item-action");
-answerList1.setAttribute("id", "answer1");
-outAnswer.append(answerList1);
+qAndABlock = function () {
 
-answerList2 = document.createElement("li");
-answerList2.textContent = qAndA.secondAnswer;
-answerList2.setAttribute("class", "list-group-item list-group-item-action")
-answerList2.setAttribute("id", "answer2");
-outAnswer.append(answerList2);
+    outQuestion.textContent = qAndA.firstQuestion;
+    outQuestion.setAttribute("class", "list-group-item list-group-item-info");
+    console.log(outQuestion);
 
-answerList3 = document.createElement("li");
-answerList3.textContent = qAndA.thirdAnswer;
-answerList3.setAttribute("class", "list-group-item list-group-item-action");
-answerList3.setAttribute("id", "answer3");
-outAnswer.append(answerList3);
+    answerList1 = document.createElement("li");
+    answerList1.textContent = qAndA.firstAnswer;
+    answerList1.setAttribute("class", "list-group-item list-group-item-action");
+    answerList1.setAttribute("id", "answer1");
+    outAnswer.append(answerList1);
 
-answerList4 = document.createElement("li");
-answerList4.textContent = qAndA.fourthAnswer;
-answerList4.setAttribute("class", "list-group-item list-group-item-action");
-answerList4.setAttribute("id", "answer4");
-outAnswer.append(answerList4);
+    answerList2 = document.createElement("li");
+    answerList2.textContent = qAndA.secondAnswer;
+    answerList2.setAttribute("class", "list-group-item list-group-item-action")
+    answerList2.setAttribute("id", "answer2");
+    outAnswer.append(answerList2);
+
+    answerList3 = document.createElement("li");
+    answerList3.textContent = qAndA.thirdAnswer;
+    answerList3.setAttribute("class", "list-group-item list-group-item-action");
+    answerList3.setAttribute("id", "answer3");
+    outAnswer.append(answerList3);
+
+    answerList4 = document.createElement("li");
+    answerList4.textContent = qAndA.fourthAnswer;
+    answerList4.setAttribute("class", "list-group-item list-group-item-action");
+    answerList4.setAttribute("id", "answer4");
+    outAnswer.append(answerList4);
+
+};
+
+
 
 //answer event listener complete
 outAnswer.addEventListener("click", function (e) {
@@ -92,70 +141,70 @@ console.log(qAndA.firstQuestion);
 // questionAndAnswer()
 // function questionAndAnswer() {
 
-    // function qABlockone() {
+// function qABlockone() {
 
-        // outQuestion.textContent = "What is the question?";
-
-
-        // var inAnswer = [
-        //     "answer 1", 
-        //     "answer 2",
-        //     "answer 3",
-        //     "answer 4",          
-        // ];
-        // console.log(inAnswer[1]);
-        // var correctAnswer = inAnswer[1];
-
-        // for (var i = 0; i < inAnswer.length; i++) {
-
-        //     var answers = inAnswer[i]; // indexing arrays splits it
-        //     var userAnswer = "";
-        //     answerList = document.createElement("li");
-        //     answerList.textContent = answers;
-        //     outAnswer.append(answerList);
-        //     answerList.setAttribute("class", "btn btn-secondary btn-lg btn-block");
-        // };     
-        // console.log(answerList);
-
-        // answerList.addEventListener("click", function(e) {      
-
-        //     if (e.target === correctAnswer) {
-
-        //         alert("correct!");
-        //     }
-        //     else {
-        //         alert("wrong!");
-        //     };
-        //     console.log(e.currentTarget);
+// outQuestion.textContent = "What is the question?";
 
 
-        // });
+// var inAnswer = [
+//     "answer 1", 
+//     "answer 2",
+//     "answer 3",
+//     "answer 4",          
+// ];
+// console.log(inAnswer[1]);
+// var correctAnswer = inAnswer[1];
+
+// for (var i = 0; i < inAnswer.length; i++) {
+
+//     var answers = inAnswer[i]; // indexing arrays splits it
+//     var userAnswer = "";
+//     answerList = document.createElement("li");
+//     answerList.textContent = answers;
+//     outAnswer.append(answerList);
+//     answerList.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+// };     
+// console.log(answerList);
+
+// answerList.addEventListener("click", function(e) {      
+
+//     if (e.target === correctAnswer) {
+
+//         alert("correct!");
+//     }
+//     else {
+//         alert("wrong!");
+//     };
+//     console.log(e.currentTarget);
 
 
+// });
 
 
 
-    // };
-
-    // function qABlockotwo() {
-
-    //     outQuestion.textContent = "What is the question?";
-    //     var inAnswer = ["Answer 1", "answer 2", "answer 3", "answer 4"];
-
-    //     for (var i = 0; i < inAnswer.length; i++) {
-
-    //         var answers = inAnswer[i]; // indexing arrays splits it
-
-    //         answerList = document.createElement("li");
-    //         answerList.textContent = answers;
-    //         outAnswer.appendChild(answerList);
-    //         answerList.setAttribute("class", "btn btn-secondary btn-lg btn-block");
-    //     };
 
 
-    // };
+// };
 
-    // return qABlockone(), qABlockotwo()
+// function qABlockotwo() {
+
+//     outQuestion.textContent = "What is the question?";
+//     var inAnswer = ["Answer 1", "answer 2", "answer 3", "answer 4"];
+
+//     for (var i = 0; i < inAnswer.length; i++) {
+
+//         var answers = inAnswer[i]; // indexing arrays splits it
+
+//         answerList = document.createElement("li");
+//         answerList.textContent = answers;
+//         outAnswer.appendChild(answerList);
+//         answerList.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+//     };
+
+
+// };
+
+// return qABlockone(), qABlockotwo()
 
 
 
@@ -167,12 +216,26 @@ console.log(qAndA.firstQuestion);
 
 
 
-//Grab player information and store it later
-// inSubmit.addEventListener("click", askName);
-// function askName() {
-//     var playerName = prompt("Please enter your name.");
-//     return inPlayer.textContent = playerName, inSubmit.textContent = "RESTART GAME", console.log(playerName), qAndA();
-// };
+// Grab player information and store it later
+inSubmit.addEventListener("click", askName);
+function askName(e) {
+    if (e.target.matches("#submit")) {
+        var playerName = prompt("Please enter your name.");
+        qAndABlock();
+        return inPlayer.textContent = playerName,
+            inSubmit.textContent = "Next Question",
+            inSubmit.setAttribute("id", "submit2"),
+            console.log(playerName);
+    }
+    if (e.target.matches("#submit2")) {
+        alert("next question!");
+    };
+
+
+
+
+};
+
 
 //create questions and they have to be random, maybe create another questions page??
 
