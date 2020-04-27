fe4e7e5ceaf2f7b6ec99e84b60f74d3e41638927
einobaka <61556425+einobaka@users.noreply.github.com>
2020-04-26 17:29:50 -0700

    added user finish button at the end of quiz and clears the board

--- script.js
+++ script.js
@@ -17,26 +17,16 @@ var qAndA = {
     fourthAnswer: "",
 
     qAndAOne: function () {
-        qAndA.firstQuestion = "question test 0 ";
-        qAndA.firstAnswer = "answer 1-0 ";
-        qAndA.secondAnswer = "answer 2-0 ";
-        qAndA.thirdAnswer = "answer 3-0 ";
-        qAndA.fourthAnswer = "answer 4-0 ";
-
-        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer + qAndA.correctAnswer;
-    },
-
-    qAndATwo: function () {
         qAndA.firstQuestion = "question test 1 ";
         qAndA.firstAnswer = "answer 1-1 ";
         qAndA.secondAnswer = "answer 2-1 ";
         qAndA.thirdAnswer = "answer 3-1 ";
         qAndA.fourthAnswer = "answer 4-1 ";
 
-        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer + qAndA.correctAnswer;
     },
 
-    qAndAThree: function () {
+    qAndATwo: function () {
         qAndA.firstQuestion = "question test 2 ";
         qAndA.firstAnswer = "answer 1-2 ";
         qAndA.secondAnswer = "answer 2-2 ";
@@ -46,7 +36,7 @@ var qAndA = {
         return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
     },
 
-    qAndAFour: function () {
+    qAndAThree: function () {
         qAndA.firstQuestion = "question test 3 ";
         qAndA.firstAnswer = "answer 1-3 ";
         qAndA.secondAnswer = "answer 2-3 ";
@@ -56,12 +46,22 @@ var qAndA = {
         return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
     },
 
-    qAndAFive: function () {
+    qAndAFour: function () {
         qAndA.firstQuestion = "question test 4 ";
         qAndA.firstAnswer = "answer 1-4 ";
         qAndA.secondAnswer = "answer 2-4 ";
         qAndA.thirdAnswer = "answer 3-4 ";
         qAndA.fourthAnswer = "answer 4-4 ";
+
+        return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
+    },
+
+    qAndAFive: function () {
+        qAndA.firstQuestion = "question test 5 ";
+        qAndA.firstAnswer = "answer 1-5 ";
+        qAndA.secondAnswer = "answer 2-5 ";
+        qAndA.thirdAnswer = "answer 3-5 ";
+        qAndA.fourthAnswer = "answer 4-5 ";
 
         return qAndA.firstQuestion + qAndA.firstAnswer + qAndA.secondAnswer + qAndA.thirdAnswer + qAndA.fourthAnswer
     },
@@ -129,8 +129,9 @@ blockTwo = function () {
     answerList4 = document.createElement("li");
     answerList4.textContent = qAndA.fourthAnswer;
     answerList4.setAttribute("class", "list-group-item list-group-item-action");
-    answerList4.setAttribute("id", "answer");
     outAnswer.append(answerList4);
+    // Correct answer insert
+    answerList4.setAttribute("id", "answer");
 
 };
 
@@ -154,8 +155,9 @@ blockThree = function () {
     answerList3 = document.createElement("li");
     answerList3.textContent = qAndA.thirdAnswer;
     answerList3.setAttribute("class", "list-group-item list-group-item-action");
-    answerList4.setAttribute("id", "answer");
     outAnswer.append(answerList3);
+    // Correct answer insert
+    answerList3.setAttribute("id", "answer");
 
     answerList4 = document.createElement("li");
     answerList4.textContent = qAndA.fourthAnswer;
@@ -164,8 +166,80 @@ blockThree = function () {
 
 };
 
+blockFour = function () {
+
+    console.log(qAndA.qAndAFour())
+    // Question four
+    outQuestion.textContent = qAndA.firstQuestion;
+    outQuestion.setAttribute("class", "list-group-item list-group-item-info");
+
+    // Answer block four - 1
+    answerList1 = document.createElement("li");
+    answerList1.textContent = qAndA.firstAnswer;
+    answerList1.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList1);
+
+    // Answer block four - 2
+    answerList2 = document.createElement("li");
+    answerList2.textContent = qAndA.secondAnswer;
+    answerList2.setAttribute("class", "list-group-item list-group-item-action")
+    outAnswer.append(answerList2);
+    // Correct answer insert
+    answerList2.setAttribute("id", "answer");
+
+    // Answer block four - 3
+    answerList3 = document.createElement("li");
+    answerList3.textContent = qAndA.thirdAnswer;
+    answerList3.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList3);
+
+    // Answer block four - 4
+    answerList4 = document.createElement("li");
+    answerList4.textContent = qAndA.fourthAnswer;
+    answerList4.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList4);
+
+};
+
+blockFive = function () {
+
+    console.log(qAndA.qAndAFive())
+    // Question five
+    outQuestion.textContent = qAndA.firstQuestion;
+    outQuestion.setAttribute("class", "list-group-item list-group-item-info");
+
+    // Answer block five - 1
+    answerList1 = document.createElement("li");
+    answerList1.textContent = qAndA.firstAnswer;
+    answerList1.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList1);
+
+    // Answer block five - 2
+    answerList2 = document.createElement("li");
+    answerList2.textContent = qAndA.secondAnswer;
+    answerList2.setAttribute("class", "list-group-item list-group-item-action")
+    // Correct answer insert
+    answerList2.setAttribute("id", "answer");
+    outAnswer.append(answerList2);
+
+    // Answer block five - 3
+    answerList3 = document.createElement("li");
+    answerList3.textContent = qAndA.thirdAnswer;
+    answerList3.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList3);
+
+    // Answer block five - 4
+    answerList4 = document.createElement("li");
+    answerList4.textContent = qAndA.fourthAnswer;
+    answerList4.setAttribute("class", "list-group-item list-group-item-action");
+    outAnswer.append(answerList4);
+
+};
+
 //answer event listener complete
+
 outAnswer.addEventListener("click", function (e) {
+    var userPoints = "";
     e.preventDefault();
     console.log(e);
     e.target.setAttribute("class", "list-group-item list-group-item-action active");
@@ -173,18 +247,24 @@ outAnswer.addEventListener("click", function (e) {
     if (e.target.matches("#answer")) {
         outResult.textContent = "Correct!"
         outResult.setAttribute("class", "alert alert-success")
+        userPoints += 20;
     }
     else {
         outResult.textContent = "Incorrect!"
         outResult.setAttribute("class", "alert alert-danger")
+        userPoints -= 20;
     };
-
-    // clear the board after
+    
+    
 });
 
+
+
+
 // Grab player information and store it later
-inSubmit.addEventListener("click", askName);
-function askName(e) {
+inSubmit.addEventListener("click", gameStart);
+function gameStart(e) {
+    //First question block and player name for storage
     if (e.target.matches("#submit")) {
         var playerName = prompt("Please enter your name.");
         blockOne();
@@ -193,6 +273,7 @@ function askName(e) {
             inSubmit.setAttribute("id", "submit1"),
             console.log(playerName);
     }
+    // Second question block
     else if (e.target.matches("#submit1")) {
         while (outAnswer.hasChildNodes()) {
             outAnswer.removeChild(outAnswer.childNodes[0]);
@@ -203,6 +284,7 @@ function askName(e) {
         outResult.setAttribute("class", "");
         inSubmit.setAttribute("id", "submit2");
     }
+    // Third question block
     else if (e.target.matches("#submit2")) {
         while (outAnswer.hasChildNodes()) {
             outAnswer.removeChild(outAnswer.childNodes[0]);
@@ -212,40 +294,43 @@ function askName(e) {
         outResult.textContent = "";
         outResult.setAttribute("class", "");
         inSubmit.setAttribute("id", "submit3");
+    }
+    // Fourth question block
+    else if (e.target.matches("#submit3")) {
+        while (outAnswer.hasChildNodes()) {
+            outAnswer.removeChild(outAnswer.childNodes[0]);
+        }
+        outAnswer.setAttribute("class", "list-group-item ");
+        blockFour();
+        outResult.textContent = "";
+        outResult.setAttribute("class", "");
+        inSubmit.setAttribute("id", "submit4");
+    }
+    // Fifth question block
+    else if (e.target.matches("#submit4")) {
+
+        while (outAnswer.hasChildNodes()) {
+            outAnswer.removeChild(outAnswer.childNodes[0]);
+        }
+        outAnswer.setAttribute("class", "list-group-item ");
+        blockFive();
+        outResult.textContent = "";
+        outResult.setAttribute("class", "");
+        return inSubmit.textContent = "FINISH",
+            inSubmit.setAttribute("id", "finish");
+
+    }
+    // Clear for score
+    else if (e.target.matches("#finish")) {
+        while (outAnswer.hasChildNodes()) {
+            outAnswer.removeChild(outAnswer.childNodes[0]);
+        }
+        outAnswer.setAttribute("class", "");
+        outQuestion.setAttribute("class", "")
+        outQuestion.textContent = "";
+        outResult.setAttribute("class", "")
+        outResult.textContent = "";
     };
 
+
 };
-//create questions and they have to be random, maybe create another questions page??
-
-// inSubmit.addEventListener("click", qAndA);
-// function qAndA() {
-//     firstQuestion = '';
-
-//     firstQuestion = document.createElement("button"),
-//     firstQuestion.textContent = "test again",
-//     console.log("this is a test");
-
-//     // outAnswer.textContent = [q1.a2, q1.a3]; 
-//     //the answer can come from a different webpage
-// };
-
-// fill a variable with button on HTML page
-
-// var q1 = {
-//     q: "question example 1",
-//     a1: "answer 1",
-//     a2: "blueblueblue2",
-//     a3: "blueblueblue3",
-// }
-
-
-
-
-
-//figure this out later
-// localStorage.setItem("player", inPlayer);
-//Replace click to start with "RESTART GAME"
-
-//Countdown of questions next to question?
-
-// have an empty bar that flashes correct or incorrect?
